f8e641c205307549b58301953f71ee8ad2dfe90c
vaporform <ysktyrch@gmail.com>
2025-11-06 13:20:42 +0700

    Reset #1

--- data_processing.py
+++ data_processing.py
@@ -21,84 +21,41 @@ def filter(condition, dict_list):
 			temps.append(item)
 	return temps
 
-# print the list of germany
+def aggregate(aggregation_key, aggregation_function, dict_list):
+	# aggregation_key: A way to group things
+	# aggregation_function: A way to combine things
+	# dict_list: your average dict/dataset.
+	info = {}
+	pass
+
+# Print all cities in Germany
 filtered_list = filter(lambda x:x['country'] == 'Germany',cities)
 print(filtered_list)
 
-'''
-# Print the average temperature of all the cities
-print("The average temperature of all the cities:")
-temps = []
-for city in cities:
-	temps.append(float(city['temperature']))
-print(sum(temps)/len(temps))
-print()
-
-# Print the average temperature of all the cities
-print("The average temperature of all the cities:")
-temps = [float(city['temperature']) for city in cities]
-print(sum(temps)/len(temps))
-print()
-
-# NOTE TO SELF:
-# 0: CITY
-# 1: COUNTRY
-# 2: LAT
-# 3: LONG
-# 4: TEMP
-# Well, you can read it as a dict so no problem... :3
-print("================================")
-
-print(cities)
-
-print("================================")
-print("ALL CITIES IN GERMANY")
-
-germany_list = []
-# Print all cities in Germany
-for entry in cities:
-	if entry['country'] == 'Germany':
-		germany_list.append(entry['city'])
-print(" ".join(germany_list))
-
-print("================================")
-print("ALL CITIES IN SPAIN AND ABOVE 12 DEGREES")
 # Print all cities in Spain with a temperature above 12°C
 
-spain_12 = []
-for entry in cities:
-	if entry['country'] == 'Spain' and float(entry['temperature']) > 12:
-		spain_12.append(entry['city'])	
+'''
+Your code here
 
-print(" ".join(spain_12))
-
+'''
 
 # Count the number of unique countries
-print("================================")
-print("UNIQUE COUNTRIES")
-c_list = []
-for entry in cities:
-	if entry['country'] not in c_list:
-		c_list.append(entry['country'])
-print(len(c_list))
 
-print("================================")
-print("AVG TEMPERATURE FOR ALL CITIES IN GERMANY")
+'''
+Your code here
+
+'''
+
 # Print the average temperature for all the cities in Germany
-a = 0
-for entry in cities:
-	if entry['country'] == 'Germany':
-		a += float(entry['temperature'])
-print(a/len(germany_list))
 
-print("================================")
-print("MAX TEMP IN ITALY")
+'''
+Your code here
+
+'''
+
 # Print the max temperature for all the cities in Italy
-italy = []
-m_temp = -float('inf')
-for entry in cities:
-	if entry['country'] == 'Italy':
-		if float(entry['temperature']) > m_temp:
-			m_temp = float(entry['temperature'])
-print(m_temp)
+
+'''
+Your code here
+
 '''
